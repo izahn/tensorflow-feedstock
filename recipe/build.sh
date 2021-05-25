d736901c5bf56858d0281f6e6b022c3692fbb7a0
Ista Zahn <izahn@hbs.edu>
2021-05-25 13:41:15 -0400

    Revert "Update recipe/build.sh"
    This reverts commit cd312384e78903725e629157f27820a07b73add5.

--- recipe/build.sh
+++ recipe/build.sh
@@ -110,7 +110,7 @@ export TF_CONFIGURE_IOS=0
 sed -i -e "/PROTOBUF_INCLUDE_PATH/c\ " .bazelrc
 sed -i -e "/PREFIX/c\ " .bazelrc
 
-export TF_CUDA_PATHS="${PREFIX},${CUDA_HOME}"
+export TF_CUDA_PATHS="${PREFIX},/usr/local/cuda-${cuda_compiler_version},/usr"
 
 ./configure
 echo "build --config=noaws" >> .bazelrc
@@ -121,3 +121,4 @@ bazel ${BAZEL_OPTS} build ${BUILD_OPTS} ${BUILD_TARGET}
 # build a whl file
 mkdir -p $SRC_DIR/tensorflow_pkg
 bash -x bazel-bin/tensorflow/tools/pip_package/build_pip_package $SRC_DIR/tensorflow_pkg
+
